6c6396d9a3ddf9b8d94e565b8223df61cc65fb7b
Amos Barbato <amosbarbato@gmail.com>
2026-03-28 21:01:26 -0300

    feat: add prisma setup

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,51 @@ import "dotenv/config";
 
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifyApiReference from "@scalar/fastify-api-reference";
 import { z } from "zod";
 
 const app = Fastify({ logger: true });
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para o bootcamp de treinos do FSC",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifyCors, {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+});
+
+await app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    sources: [
+      { title: "Trainer AI API", slug: "trainer-ai-api", url: "/swagger.json" },
+    ],
+  },
+});
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
