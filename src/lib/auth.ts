cada4df5ae90208050b533702e17bbbf77bedc49
Amos Barbato <amosbarbato@gmail.com>
2026-03-30 11:54:56 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,15 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "./db.js";
 
 export const auth = betterAuth({
+  baseURL: process.env.API_BASE_URL,
   trustedOrigins: ["http://localhost:3000"],
   emailAndPassword: { enabled: true },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   database: prismaAdapter(db, {
     provider: "postgresql",
   }),
