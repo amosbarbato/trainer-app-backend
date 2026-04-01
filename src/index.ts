12228f3c58b3934ccea42c7ff038f2bc7e88541e
Amos Barbato <amosbarbato@gmail.com>
2026-04-01 14:43:55 -0300

    chore: update build command and host

--- src/index.ts
+++ src/index.ts
@@ -149,7 +149,7 @@ app.route({
 });
 
 try {
-  await app.listen({ port: env.PORT });
+  await app.listen({ host: "0.0.0.0", port: env.PORT });
 } catch (error) {
   app.log.error(error);
   process.exit(1);
